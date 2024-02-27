29d7de6f207d27e7184c07cf36ab5123f45199a5
Rayan Kotob <rayan.kotob22@gmail.com>
2024-02-27 16:00:44 +0100

    Window attempt

--- Engine/D3DApp.h
+++ Engine/D3DApp.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 namespace ave {
 	class D3DApp {
@@ -10,8 +11,24 @@ namespace ave {
 		D3DApp& operator=(const D3DApp& rhs) = delete;
 		virtual ~D3DApp();
 
+		// Getter 
+		static D3DApp* GetApp();
+		HINSTANCE AppInst()const;
+		HWND      MainWnd()const;
+
 		void Init(HINSTANCE hInstance);
 		bool InitMainWindow();
 		bool InitDirect3D();
+
+	protected:
+
+		static D3DApp* mApp;
+
+		HINSTANCE mhAppInst = nullptr;
+		HWND      mhMainWnd = nullptr;
+
+		std::wstring mMainWndCaption = L"d3d App";
+		int mClientWidth = 800;
+		int mClientHeight = 600;
 	};
 }
