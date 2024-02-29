9f6b64fe1ce0f7860d0f48c62f60b4c38fe60520
nleguyader <nleguyader@gaming.tech>
2024-02-29 15:39:48 +0100

    ça marche

--- Engine/Transform.cpp
+++ Engine/Transform.cpp
@@ -32,11 +32,11 @@ namespace ave {
 		m_bHandleChange = true;
 	}
 
-	void Transform::Move(float fX, float fY, float fZ){
-		m_vPosition.x = fX;
-		m_vPosition.y = fY;
-		m_vPosition.z = fZ;
-		XMStoreFloat4x4(&m_mRotation, XMMatrixTranslation(m_vPosition.x, m_vPosition.y, m_vPosition.z));
+	void Transform::Move(float fX, float fY, float fZ) {
+		m_vPosition.x += fX;
+		m_vPosition.y += fY;
+		m_vPosition.z += fZ;
+		XMStoreFloat4x4(&m_mPosition, XMMatrixTranslation(m_vPosition.x, m_vPosition.y, m_vPosition.z));
 	}
 
 	void Transform::Scale(XMFLOAT3* pvScale) {
@@ -45,10 +45,10 @@ namespace ave {
 	}
 
 	void Transform::Scale(float fX, float fY, float fZ) {
-		m_vScale.x = fX;
-		m_vScale.y = fY;
-		m_vScale.z = fZ;
-		XMStoreFloat4x4(&m_mScale, XMMatrixTranslation(m_vScale.x, m_vScale.y, m_vScale.z));
+		m_vScale.x += fX;
+		m_vScale.y += fY;
+		m_vScale.z += fZ;
+		XMStoreFloat4x4(&m_mScale, XMMatrixScaling(m_vScale.x, m_vScale.y, m_vScale.z));
 	}
 
 	void Transform::Rotate(XMFLOAT3* pvRotate) {
@@ -60,23 +60,22 @@ namespace ave {
 
 		XMVECTOR qQuaternion;
 		qQuaternion = XMQuaternionRotationAxis(XMLoadFloat3(&m_vDir), fRoll);
-		qQuaternion *= XMQuaternionRotationAxis(XMLoadFloat3(&m_vRight), fPitch);
-		qQuaternion *= XMQuaternionRotationAxis(XMLoadFloat3(&m_vUp), fYaw);
+		qQuaternion = XMQuaternionMultiply(qQuaternion, XMQuaternionRotationAxis(XMLoadFloat3(&m_vRight), fPitch));
+		qQuaternion = XMQuaternionMultiply(qQuaternion, XMQuaternionRotationAxis(XMLoadFloat3(&m_vUp), fYaw));
 
-		XMStoreFloat4(&m_qRotation, XMLoadFloat4(&m_qRotation) * qQuaternion);
-		
-		XMFLOAT4X4 mRotation;
-		XMStoreFloat4x4(&mRotation, XMMatrixRotationQuaternion(XMLoadFloat4(&m_qRotation)));
-		m_vRight.x = mRotation._11;
-		m_vRight.y = mRotation._12;
-		m_vRight.z = mRotation._13;
-		m_vUp.x = mRotation._21;
-		m_vUp.y = mRotation._22;
-		m_vUp.z = mRotation._23;
-		m_vDir.x = mRotation._31;
-		m_vDir.y = mRotation._32;
-		m_vDir.z = mRotation._33;
+		XMStoreFloat4(&m_qRotation, XMQuaternionMultiply(XMLoadFloat4(&m_qRotation), qQuaternion));
 
+		XMStoreFloat4x4(&m_mRotation, XMMatrixRotationQuaternion(XMLoadFloat4(&m_qRotation)));
+
+		m_vRight.x = m_mRotation._11;
+		m_vRight.y = m_mRotation._12;
+		m_vRight.z = m_mRotation._13;
+		m_vUp.x = m_mRotation._21;
+		m_vUp.y = m_mRotation._22;
+		m_vUp.z = m_mRotation._23;
+		m_vDir.x = m_mRotation._31;
+		m_vDir.y = m_mRotation._32;
+		m_vDir.z = m_mRotation._33;
 	}
 
 	void Transform::UpdateMatrice() {
@@ -84,9 +83,10 @@ namespace ave {
 		XMMATRIX mScale = XMLoadFloat4x4(&m_mScale);
 		XMMATRIX mPosition = XMLoadFloat4x4(&m_mPosition);
 		XMMATRIX mRotation = XMLoadFloat4x4(&m_mRotation);
-		XMMATRIX mWorld = mScale * mRotation * mPosition;
+		XMMATRIX mWorld = XMMatrixMultiply(XMMatrixMultiply(mScale, mRotation), mPosition);
 
 		XMStoreFloat4x4(&m_mTransformation, mWorld);
+		m_bHandleChange = false;
 	}
 
 	Transform::~Transform(){
