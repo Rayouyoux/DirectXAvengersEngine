884f64c6e6dd48cdb6522f17c212d2f331260af2
Rayan Kotob <rayan.kotob22@gmail.com>
2024-03-05 12:34:16 +0100

    Input Manager Restarted, Keyboard detection added

--- Engine/InputManager.h
+++ Engine/InputManager.h
@@ -13,37 +13,11 @@ namespace ave {
         ~InputManager();
 
         // Main methods
-        void Initialize(HWND hwnd);
-        void Update();
-
-        // Methods for handling mouse input
-        void OnMouseMove(int x, int y);
-        void OnMouseDown(int button, HWND hwnd);
-        void OnMouseUp(int button);
+        void Initialize();
 
         // Methods for handling keyboard input
-        void OnKeyDown(unsigned char key);
-        void OnKeyUp(unsigned char key);
-
-    private:
-
-        // Camera Control Methods
-        void RotateCamera(float dx, float dy);
-        void MoveCamera(float dx, float dy, float dz);
-
-    private:
-
-        // Camera Control Variables
-        DirectX::XMFLOAT3 m_cameraPosition;
-        DirectX::XMFLOAT3 m_cameraRotation;
-
-        // Mouse 
-        bool m_mouseCaptured;
-        int m_lastMouseX;
-        int m_lastMouseY;
-
-        // Keyboard
-        bool m_keyState[256];
+        int IsKeyDownI();
+        char IsKeyDownC();
 
 	};
 }
